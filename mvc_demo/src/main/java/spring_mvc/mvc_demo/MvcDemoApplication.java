d94c5ac2648d688b70a7a59eb86d342107b127f8
prajakta1104 <114208219+prajakta1104@users.noreply.github.com>
2022-10-06 17:23:25 +0530

    Add files via upload

---
+++ mvc_demo/src/main/java/spring_mvc/mvc_demo/MvcDemoApplication.java
@@ -0,0 +1,13 @@
+package spring_mvc.mvc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcDemoApplication.class, args);
+	}
+
+}
